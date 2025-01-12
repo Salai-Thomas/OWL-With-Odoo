1f0278277ae0008335a69e18aaf547083af06560
SalaiThomas <salaithomas582@gmail.com>
2025-01-12 17:55:26 +0630

    dynamic class attibute

--- todo_list/static/src/todolist/todolist.js
+++ todo_list/static/src/todolist/todolist.js
@@ -12,7 +12,7 @@ export class TodoList extends Component{
     setup(){
     this.todos = useState(
     [
-    { id: 1, description: "buy milk", isCompleted: false },
+    { id: 1, description: "buy milk", isCompleted: true },
     { id: 2, description: "buy coffee", isCompleted: false },
     { id: 3, description: "buy sugar", isCompleted: false },
     ]
