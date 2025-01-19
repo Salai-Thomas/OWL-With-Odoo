c5f0c92988e9e1ef166f277c9d56935cc099fadc
SalaiThomas <salaithomas582@gmail.com>
2025-01-19 18:18:39 +0630

    fixed props passing

--- todo_list/static/src/todoitem/todoitem.js
+++ todo_list/static/src/todoitem/todoitem.js
@@ -5,5 +5,6 @@ import {Component,useState} from "@odoo/owl"
 export class TodoItem extends Component{
     static template = "todolist.TodoItem"
 
-    static props = ['todos']
+    static props = ['todo']
+
 }
--- todo_list/static/src/todolist/todolist.js
+++ todo_list/static/src/todolist/todolist.js
@@ -18,6 +18,7 @@ export class TodoList extends Component{
     ]
     );
 
+
     }
 
     addTodo(ev){
