f67cff8e4a9ddd77dcb370e4ca6e5078a3fc29c2
SalaiThomas <salaithomas582@gmail.com>
2025-01-26 00:16:30 +0630

    using Layout component

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,10 +1,15 @@
 /** @odoo-module **/
 
 import { Component } from "@odoo/owl";
+import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
+
+    static components = {Layout}
 }
+
+
 
 registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
