0c9753c715ef33d0d0b126f7fbbc8d1c93ba6e10
SalaiThomas <salaithomas582@gmail.com>
2025-01-26 21:47:48 +0630

    add Two action button

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -3,11 +3,34 @@
 import { Component } from "@odoo/owl";
 import { Layout } from "@web/search/layout";
 import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { _t } from "@web/core/l10n/translation";
+
+
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
 
     static components = {Layout}
+
+    setup() {
+      this.action = useService("action");
+    }
+
+    openCustomer() {
+      this.action.doAction("base.action_partner_form");
+}
+
+    async openCRM(activity) {
+        this.action.doAction({
+            type: 'ir.actions.act_window',
+            name: _t('CRM'),
+            target: 'current',
+            res_id: activity.res_id,
+            res_model: 'crm.lead',
+            views: [[false, 'form','list']],
+        });
+    }
 }
 
 
