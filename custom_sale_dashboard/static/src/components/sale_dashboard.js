5f0ac9e264d7a3fb8df0ad0486a90c5056bc51dc
SalaiThomas <salaithomas582@gmail.com>
2025-02-15 16:01:32 +0630

    Create Kpi_card Component and apply

--- custom_sale_dashboard/static/src/components/sale_dashboard.js
+++ custom_sale_dashboard/static/src/components/sale_dashboard.js
@@ -2,10 +2,11 @@
 
 import { registry } from "@web/core/registry";
 import { Component } from  "@odoo/owl";
+import { KpiCard } from "./Kpi_card/kpi_card"
 
 class SaleDashboardAction extends Component {
     static template = "custom_sale_dashboard.SaleDashboardAction";
 }
 
-// remember the tag name we put in the first step
+SaleDashboardAction.components = {KpiCard}
 registry.category("actions").add("custom_sale_dashboard.sale_dashboard", SaleDashboardAction);
