b276bab3e343d8e59990e144f99da0b72b4b9826
SalaiThomas <salaithomas582@gmail.com>
2025-02-15 20:12:31 +0630

    use char.js cdn by using loadJS

--- custom_sale_dashboard/static/src/components/sale_dashboard.js
+++ custom_sale_dashboard/static/src/components/sale_dashboard.js
@@ -1,11 +1,17 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { Component } from  "@odoo/owl";
+import { loadJS } from "@web/core/assets";
+import { Component,onWillStart,useRef,onMounted } from  "@odoo/owl";
 import { KpiCard } from "./Kpi_card/kpi_card"
 
 class SaleDashboardAction extends Component {
     static template = "custom_sale_dashboard.SaleDashboardAction";
+    setup(){
+        onWillStart(async()=>{
+        await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js")
+        })
+    }
 }
 
 SaleDashboardAction.components = {KpiCard}
